2b87f90ceead5082f5d78cf3ed4ccc55ffb1df87
Prithvi Balaji <prithvibalaji16@gmail.com>
2023-09-13 20:52:24 -0500

    Update 1.2.py

--- 1.2/1.2.py
+++ 1.2/1.2.py
@@ -1,1 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 5
+
+temperature_readings = [25.3, 25.4, 25.1, 25.8, 26.2, 25.7, 26.0, 25.9, 25.5, 25.4]
+
+
+averaged_readings = []
+
+
+for i in range(len(temperature_readings)):
+    if i < N:
+      
+        averaged_value = temperature_readings[i]
+    else:
+
+        averaged_value = np.mean(temperature_readings[i - N : i + 1])
+    
+    averaged_readings.append(averaged_value)
+
+
+time = np.arange(len(temperature_readings))
+
+
+plt.figure(figsize=(10, 5))
+plt.plot(time, temperature_readings, label='Original Readings', marker='o')
+plt.plot(time, averaged_readings, label=f'Averaged (N={N})', marker='x')
+plt.xlabel('Time')
+plt.ylabel('Temperature')
+plt.title('Temperature Readings vs. Time')
+plt.legend()
+plt.grid(True)
+
+
+plt.show()
 
